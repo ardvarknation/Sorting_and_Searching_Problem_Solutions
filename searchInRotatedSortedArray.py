f30b36825ddbcf2b33ea560bc2173ba25b2a53bf
richard-maree <41262799+ardvarknation@users.noreply.github.com>
2026-08-31 19:23:31 +0100

    Add start of solution outline for search in rotated array (TBC)
    Added a solution outline for the LeetCode problem 'Search in a Rotated Sorted Array', including problem description and constraints.

---
+++ searchInRotatedSortedArray.py
@@ -0,0 +1,19 @@
+# Solution for LeetCode problem "Search in a Rotated Sorted Array".
+# Description:
+#  There is an integer array nums sorted in ascending order (with distinct values).
+#  Prior to be being passed to the function, nums is possibly left rotated at an unknown
+#  index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ...,
+#  nums[n-1], nums[0], nums[1], nums[k-1]] (0-indexed). E.g. [0, 1, 2, 4, 5, 6, 7] might 
+#  be left rotated by 3 indices and become [4, 5, 6, 7, 0, 1, 2].
+#  Given the array nums after the possible rotation and an integer target, return the 
+#  index of the target if it is in nums or -1 if it is not.
+#  The algorithm must run in O(log n) runtime complexity.
+
+# Constraints:
+# - 1 <= nums.length <= 5000
+# - -10^4 <= nums[i] <= 10^4
+# - All values of nums are unique
+# - nums is an ascending array possibly rotated
+# - -10^4 <= target <= 10^4
+
+#####TOBECOMPLETED#############
